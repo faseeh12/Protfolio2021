2f65085d96d7915b56dee697b04a0a871b42b2e1
Faseeh <faseehyasin12@gmail.com>
2021-06-07 17:28:30 +0500

    All work done now will work on some minor changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 AOS.init({
     duration: 1400
 });
-var option = {
-    damping: 0.03
-  }
-  Scrollbar.init(document.querySelector('#my-scrollbar'), option);
+$(function () {
+  $.srSmoothscroll({
+    target: $('#widget1'),
+    container: $('#container1')
+  })
+})
