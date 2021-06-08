99b4818a5ab9ee34b73c6a41389e787a6f4b12b3
Faseeh <faseehyasin12@gmail.com>
2021-06-08 16:15:18 +0500

    worked on companies section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+$(document).ready(function(){
+  function loaded(){
+    $('.loader').css('display', 'none');
+  }
+  loaded();
+
+  function animatePath(pathname, animation) {
+    var path = document.querySelector(pathname);
+    var length = path.getTotalLength();
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition =
+      'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      animation;
+    // Go!
+    path.style.strokeDashoffset = '0';
+  }
+  
+  animatePath('#LF path', 'stroke-dashoffset 1s ease-in-out');
+  animatePath('#LF-shadow path', 'stroke-dashoffset 1s ease-in-out');
+  animatePath('#enton path', 'stroke-dashoffset 2s 1s ease-in-out');
+  animatePath('#enton-shadow path', 'stroke-dashoffset 2s 1s ease-in-out');
+})
+
 AOS.init({
     duration: 1400
 });
@@ -7,3 +38,5 @@ $(function () {
     container: $('#container1')
   })
 })
+
+
